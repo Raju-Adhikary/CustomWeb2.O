05532d576b5eac531a252a2685224317053047c9
Raju Adhikary <adhikaryraju366@gmail.com>
2023-08-09 22:49:27 +0530

    add comp 1.0000

---
+++ js-functions/require.js
@@ -0,0 +1,13 @@
+(function (g, n, f) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = f():
+    typeof define === 'function' && define.amd ? define(f):
+    (g = typeof globalThis !== 'undefined' ? globalThis: g || self, g[n] = f());
+})(this, "module_name", (function () {
+        'use strict';
+        
+        
+        let index = {
+            
+        };
+        return index;
+    }));
